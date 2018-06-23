4d054e8dc42dbdc98d81eded08c2dcdfe8869478
student38 <student38@pcoip.ki.agh.edu.pl>
2018-06-23 14:05:48 +0200

    Next step in FileFinder. Still not ready

--- reporter/src/main/java/reporter/FileFinder.java
+++ reporter/src/main/java/reporter/FileFinder.java
@@ -7,6 +7,8 @@ import java.util.Collections;
 public class FileFinder {
 	
 	private ArrayList<File> fileList;
+	private String root;
+	private File rootFile = new File(root);
 
 	private File[] listFilesFromDirectory(File dir) {
 		return dir.listFiles();
@@ -16,8 +18,22 @@ public class FileFinder {
 		Collections.addAll(fileList, vals);
 	}
 	
-	public FileFinder(String root) {
+	private void generateFileList(File directory) {
 		
+		if(directory.isDirectory()) {
+			for (File f : directory.listFiles()) {
+				generateFileList(f);
+			}
+		}
+		else {
+			fileList.add(f);
+		}
+			
+	}
+	
+	public FileFinder(String root) {
+		this.root = root;
+		this.fileList = new ArrayList<File>();
 	}
 	
 	public ArrayList<File> getFileList() {
