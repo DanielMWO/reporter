5f883593d2294cc97be4012db911123134146ca7
Tomasz Skora <tskora93@gmail.com>
2018-06-24 10:29:34 +0200

    stub output of raport2 function

--- reporter/src/main/java/reporter/Report2.java
+++ reporter/src/main/java/reporter/Report2.java
@@ -2,6 +2,7 @@ package reporter;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Report2 implements IReport {
 	
@@ -9,15 +10,37 @@ public class Report2 implements IReport {
 	ArrayList<Record> records = repository.getRecords();
 
 	@Override
-	public RaportOutput getReport(Date date, String task, String projectName, String firstName, String lastName) {
+	public RaportOutput getReport(ArrayList<Record> records, HashMap<String, String> options) {
 
+		HashMap<String, Integer> projectsAndHours = new HashMap<String, Integer>();
 		
+		for (Record record : records) {
+			consumeLine(record, projectsAndHours);
+		}
 		
-		return null;
+		ArrayList<RaportField> raportField = new ArrayList<RaportField>();
+		
+		for (String projectName : projectsAndHours.keySet()) {
+			int projectHours = projectsAndHours.get(projectName);
+			//RaportField raportField = new RaportField(projectName, projectHours);
+		}
+		
+		return new RaportOutput(null, null);
 	}
 	
-	public static void main(String[] args) {
-		
+	private void consumeLine(Record record, HashMap<String, Integer> projectsAndHours) {
+		if (projectsAndHours.containsKey(record.ProjectName)) {
+			int oldValue = projectsAndHours.get(record.ProjectName);
+			int newValue = oldValue + record.Hours;
+			projectsAndHours.replace(record.ProjectName, newValue);
+		}
+		else {
+			projectsAndHours.put(record.ProjectName, record.Hours);
+		}
 	}
+
+	/*public static void main(String[] args) {
+		
+	}*/
 
 }
