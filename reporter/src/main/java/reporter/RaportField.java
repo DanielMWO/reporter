fc411b9e87decb0efcbbe8ce48f15fcc9ab7c56c
Tomasz Skora <tskora93@gmail.com>
2018-06-24 09:43:37 +0200

    Model classes for Raport output

---
+++ reporter/src/main/java/reporter/RaportField.java
@@ -0,0 +1,14 @@
+package reporter;
+
+public class RaportField {
+	
+	String raportUnit;
+	
+	int numberOfHours;
+	
+	public RaportField(String raportUnit, int numberOfHours) {
+		this.raportUnit = raportUnit;
+		this.numberOfHours = numberOfHours;
+	}
+
+}
