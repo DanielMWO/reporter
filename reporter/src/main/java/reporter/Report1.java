5f438cb1e7cd6eaa9153073e675e771090725dde
Tomasz Skora <tskora93@gmail.com>
2018-06-24 11:07:47 +0200

    Raport1 probably works. Redundant print from raport2 deleted. Small change in example data from repository.

--- reporter/src/main/java/reporter/Report1.java
+++ reporter/src/main/java/reporter/Report1.java
@@ -2,17 +2,56 @@ package reporter;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Report1 implements IReport{
 	
-	ArrayList<Record> records = Repository.getRecords();
-	
-	public RaportOutput getReport(Date date, String task, String projectName, String firstName, String lastName) {
+	@Override
+	public RaportOutput getReport(ArrayList<Record> records, HashMap<String, String> options) {
+
+		HashMap<String, Integer> peopleAndHours = new HashMap<String, Integer>();
 		
+		for (Record record : records) {
+			consumeLine(record, peopleAndHours);
+		}
 		
-	
+		ArrayList<RaportField> raportFields = new ArrayList<RaportField>();
 		
-		return null;
+		for (String personName : peopleAndHours.keySet()) {
+			int projectHours = peopleAndHours.get(personName);
+			RaportField raportField = new RaportField(personName, projectHours);
+			raportFields.add(raportField);
+		}
+				
+		return new RaportOutput("Raport 1 \n Projekt \t liczba godzin", raportFields);
 	}
+	
+	private void consumeLine(Record record, HashMap<String, Integer> projectsAndHours) {
+		String nameAndSurname = record.FirstName+" "+record.LastName;
+		if (projectsAndHours.containsKey(nameAndSurname)) {
+			int oldValue = projectsAndHours.get(nameAndSurname);
+			int newValue = oldValue + record.Hours;
+			projectsAndHours.replace(nameAndSurname, newValue);
+		}
+		else {
+			projectsAndHours.put(nameAndSurname, record.Hours);
+		}
+	}
+
+	/*public static void main(String[] args) {
+		
+		Repository repository = new Repository();
+		ArrayList<Record> records = repository.getRecords();
+		
+		Report1 report1 = new Report1();
+		RaportOutput raportOutput = report1.getReport(records, null);
+		
+		System.out.println(raportOutput.getHeader());
+		
+		for (RaportField raportField : raportOutput.getRaportFields()) {
+			System.out.println(raportField.getRaportUnit()+"\t"+raportField.getNumberOfHours());
+		}
+		
+	}*/
 
 }
