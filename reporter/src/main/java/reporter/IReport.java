27ecbd6e6793061ea8a19bfa445ec0360cedc975
Tomasz Skora <tskora93@gmail.com>
2018-06-24 10:06:38 +0200

    Report interface now has a function that takes as parameters arraylist and hashmap

--- reporter/src/main/java/reporter/IReport.java
+++ reporter/src/main/java/reporter/IReport.java
@@ -2,10 +2,10 @@ package reporter;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public interface IReport {
 	
-	
-	public RaportOutput getReport (Date date, String task, String projectName, String firstName, String lastName);;
+	public RaportOutput getReport(ArrayList<Record> records, HashMap<String, String> options);
 
 }
